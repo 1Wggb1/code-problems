96312538778829e6df564f8f102e9704b9a33fe4
1Wggb1 <willianggbatista@gmail.com>
2025-01-01 19:30:45 -0300

    Leet code problems 100, 110 - Tree

---
+++ src/main/java/br/com/codeproblems/tree/BalancedTreeProblem110.java
@@ -0,0 +1,29 @@
+package br.com.codeproblems.tree;
+
+public class BalancedTreeProblem110 {
+
+    public static void main(String[] args) {
+        System.out.println(BalancedTreeProblem110.isBalanced(new TreeNode(1, null, new TreeNode(2, null, new TreeNode(3)))));
+    }
+
+    public static boolean isBalanced(TreeNode root) {
+        try {
+            validateIfIsBalanced(root);
+            return true;
+        } catch (Exception e) {
+            return false;
+        }
+    }
+
+    private static int validateIfIsBalanced(TreeNode root) {
+        if(root == null) {
+            return 0;
+        }
+        int left = root.left != null ? 1 + validateIfIsBalanced(root.left) : 0;
+        int right = root.right != null ? 1 + validateIfIsBalanced(root.right) : 0;
+        if(Math.abs(left - right) > 1) {
+            throw new RuntimeException("Not balanced tree");
+        }
+        return Math.max(left, right);
+    }
+}
---
+++ src/main/java/br/com/codeproblems/tree/SameTreeProblem100.java
@@ -0,0 +1,17 @@
+package br.com.codeproblems.tree;
+
+public class SameTreeProblem100 {
+    public static void main(String[] args) {
+        System.out.println(SameTreeProblem100.isSameTree(new TreeNode(1, null, new TreeNode(2, null, new TreeNode(3))), null));
+    }
+
+    public static boolean isSameTree(TreeNode p, TreeNode q) {
+        if(p == null || q == null){
+            return p == null && q == null;
+        }
+        if(p.val != q.val) {
+            return false;
+        }
+        return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+    }
+}
